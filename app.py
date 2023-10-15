0c7ec4d9807c9a82a6c23f0ba94a9f91441149f1
maru83 <rakkunmaru@icloud.com>
2023-10-15 16:34:48 +0900

    生成ボタンを追加

--- app.py
+++ app.py
@@ -2,24 +2,28 @@
 #乱数を用いて、5×5マスのビンゴカードを生成する
 #生成したカードを表示する
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 
 
-# ビンゴカードの各列ごとに乱数を生成
-B = random.sample(range(1, 16), k=5)
-I = random.sample(range(16, 31), k=5)
-N = random.sample(range(31, 46), k=5)
-G = random.sample(range(46, 61), k=5)
-O = random.sample(range(61, 76), k=5)
-
-# ビンゴカードの数字をHTMLに渡す
-@app.route('/')
+# ビンゴカードの数字をhtmlに渡す
+@app.route('/', methods=['GET', 'POST'])
 def generate_bingo_card():
-    return render_template('bingo_card.html', B=B, I=I, N=N, G=G, O=O)
-
+    if request.method == 'POST':
+        # ビンゴカードの各列ごとに乱数を生成
+        B = random.sample(range(1, 16), k=5)
+        I = random.sample(range(16, 31), k=5)
+        N = random.sample(range(31, 46), k=5)
+        G = random.sample(range(46, 61), k=5)
+        O = random.sample(range(61, 76), k=5)
+        return render_template('bingo_card.html', B=B, I=I, N=N, G=G, O=O)
+    
+    else:
+        return render_template('bingo_card.html', B=None, I=None, N=None, G=None, O=None)
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
